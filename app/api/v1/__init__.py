dad358456090e9e00ae1dd4264025e0fe6021bb5
bt.bettertrans <bt.bettertrans@gmail.com>
2025-05-01 00:01:37 +0800

    feature/自動把所有的 router_*.py 掛進來

---
+++ app/api/v1/__init__.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+import importlib
+import pkgutil
+
+router = APIRouter()
+
+for _, module_name, _ in pkgutil.iter_modules(__path__):
+    if module_name.startswith("route_"):
+        module = importlib.import_module(f"{__name__}.{module_name}")
+        router.include_router(module.router)
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1.route_example import router as example_router
+from app.api.v1 import router as v1_router
 
 app = FastAPI()
 
@@ -8,4 +8,6 @@ def root():
     return {"message": "Hello World"}
 
 # 把 v1 版的 API router 掛進來
-app.include_router(example_router, prefix="/api/v1", tags=["Example"])
+# app.include_router(example_router, prefix="/api/v1", tags=["Example"])
+# 自動把所有的 v1 版 API router 掛進來
+app.include_router(v1_router, prefix="/api/v1")
